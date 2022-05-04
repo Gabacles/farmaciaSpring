032b2e5d31d790347ee426a136c7be71c8957cdb
Gabacles <g.dalrovere@gmail.com>
2022-05-04 10:30:14 -0300

    Atividade back-end 04.05.22

---
+++ farmacia/src/main/java/com/farmamedc/farmacia/model/Produto.java
@@ -0,0 +1,54 @@
+package com.farmamedc.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name= "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@NotNull
+	@Size(min = 1, max = 100, message="O campo deve ter no mínimo 10 caracteres, e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotNull
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
---
+++ farmacia/src/test/java/com/farmamedc/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmamedc.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
